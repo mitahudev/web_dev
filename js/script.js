390b99141f52050a02d28e6f618deaf7340f9b06
mitahudev <115945235+mitahudev@users.noreply.github.com>
2023-01-26 07:12:11 +0700

    RsydFhmyCoffee

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Toggle Kelas aktif
+const navbarNav = document.querySelector(".navbar-nav");
+// Ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik Diluar sidebar Untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+//
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
